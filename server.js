49ea0d82e52cec861b3836d89dc1725345b7734b
Martin Papa <martin.papa@outlook.com>
2019-12-17 09:57:45 -0800

    stopping point

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+function Book(title = ',', author='', description='', url) {
+  this.title = title;
+  this.title = author;
+  this.description = description;
+  this.url = "https" + url.substring(4);
+
+}
 
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +19,14 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('pages/index');
+  res.render('pages/index');
+})
+
+app.post('/searches/show', (req, res) => {
+  console.log(req.body);
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.search_criteria}+in${req.body.search_criteria}:${req.body.search}`).then(data => {
+    console.log(data);
+  })
 })
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
