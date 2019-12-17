807fde76493c64f15300222faa0dbf4dcb76f7ec
Martin Papa <martin.papa@outlook.com>
2019-12-17 09:21:49 -0800

    completed task one

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const superagent = require('superagent');
 const ejs = require('ejs');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 
 app.use(express.static('./public'));
-app.use(express.urlencoded( {extended:true} ));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-
-
+app.get('/hello', (req, res) => {
+    res.render('pages/index');
+})
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// app.get('*', (request, response) => response.status(404).send('This route does not exist'));
